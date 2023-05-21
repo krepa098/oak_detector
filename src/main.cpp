123800e964ae1ed8759543496cff19558c2327c2
Paul <krepa098@gmail.com>
2023-05-21 12:05:58 +0200

    sys info, nn, depth, encoding

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "oak_detector/pipeline.h"
+
 #include "rclcpp/rclcpp.hpp"
 
 #include "depthai/pipeline/datatypes.hpp"
@@ -13,9 +14,30 @@
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
+#include "depthai_ros_msgs/msg/spatial_detection.hpp"
+#include "depthai_ros_msgs/msg/spatial_detection_array.hpp"
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("oak_detector_node");
+  auto compressed_image_pub = node->create_publisher<sensor_msgs::msg::CompressedImage>(
+      "color/compressed", rclcpp::QoS(10).best_effort());
+
+  auto preview_pub = node->create_publisher<sensor_msgs::msg::Image>("preview/image",
+                                                                     rclcpp::QoS(10).best_effort());
+
+  auto compressed_mono_pub = node->create_publisher<sensor_msgs::msg::CompressedImage>(
+      "mono/compressed", rclcpp::QoS(10).best_effort());
+
+  auto depth_pub =
+      node->create_publisher<sensor_msgs::msg::Image>("depth/image", rclcpp::QoS(10).best_effort());
+
+  auto object_tracker_pub = node->create_publisher<depthai_ros_msgs::msg::SpatialDetectionArray>(
+      "object_tracker/detections", rclcpp::QoS(10).best_effort());
 
   RCLCPP_INFO(node->get_logger(), "creating pipeline");
   dai::Pipeline pipeline = createPipeline(BLOB_PATH);
@@ -24,33 +46,158 @@ int main(int argc, char **argv) {
   dai::Device device(pipeline);
   auto cal = device.getDeviceInfo();
 
+  // enable laser projector
+  device.setIrLaserDotProjectorBrightness(0.0);
+  // device.setLogLevel(dai::LogLevel::DEBUG);
+  // device.setLogOutputLevel(dai::LogLevel::DEBUG);
+
   // create queues
   // each queue has its own thread
   RCLCPP_INFO(node->get_logger(), "creating output queues");
-  std::shared_ptr<dai::DataOutputQueue> preview_queue =
-      device.getOutputQueue("preview", 30, false);
+  std::shared_ptr<dai::DataOutputQueue> sys_logger_queue =
+      device.getOutputQueue("sys_logger", 4, false);
+  std::shared_ptr<dai::DataOutputQueue> preview_queue = device.getOutputQueue("preview", 15, false);
   std::shared_ptr<dai::DataOutputQueue> nnet_data_qeue =
-      device.getOutputQueue("detections", 30, false);
+      device.getOutputQueue("detections", 4, false);
+  std::shared_ptr<dai::DataOutputQueue> color_encoder_queue =
+      device.getOutputQueue("color_mjpeg", 30, false);
+  std::shared_ptr<dai::DataOutputQueue> mono_encoder_queue =
+      device.getOutputQueue("mono_mjpeg", 15, false);
+  std::shared_ptr<dai::DataOutputQueue> depth_queue = device.getOutputQueue("depth", 15, false);
+  std::shared_ptr<dai::DataOutputQueue> tracker_queue =
+      device.getOutputQueue("object_tracker", 4, false);
 
   // ros bridge
   dai::rosBridge::ImageConverter img_converter("rgb_frame");
-  auto rgb_cam_info = img_converter.calibrationToCameraInfo(
-      device.readCalibration(), dai::CameraBoardSocket::RGB);
+  auto rgb_cam_info =
+      img_converter.calibrationToCameraInfo(device.readCalibration(), dai::CameraBoardSocket::RGB);
 
-  //   nnet_data_qeue->addCallback([&](std::shared_ptr<dai::ADatatype> callback)
-  //   {
-  //     if (auto nn_data = dynamic_cast<dai::NNData *>(callback.get())) {
-  //     }
-  //   });
+  //   dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame>
+  //       rgb_publish(preview_queue, node, std::string("color/image"),
+  //                   std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
+  //                             &img_converter, std::placeholders::_1,
+  //                             std::placeholders::_2),
+  //                   30, rgb_cam_info, "color");
 
-  dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame>
-      rgb_publish(preview_queue, node, std::string("color/image"),
-                  std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
-                            &img_converter, std::placeholders::_1,
-                            std::placeholders::_2),
-                  30, rgb_cam_info, "color");
+  //   dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame>
+  //       mjpeg_publish(encoder_queue, node,
+  //       std::string("color/image/compressed"),
+  //                     std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
+  //                               &img_converter, std::placeholders::_1,
+  //                               std::placeholders::_2),
+  //                     30);
 
-  rgb_publish.addPublisherCallback();
+  sys_logger_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (auto buf = dynamic_cast<dai::SystemInformation *>(callback.get())) {
+      RCLCPP_INFO(node->get_logger(),
+                  "SysInfo\nCPU  | Leon CSS %.1f%%, Leon MSS %.1f%%\nTEMP | Chip %.1f°C",
+                  buf->leonCssCpuUsage.average * 100.0f, buf->leonMssCpuUsage.average * 100.0f,
+                  buf->chipTemperature.average);
+    }
+  });
+
+  color_encoder_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (compressed_image_pub->get_subscription_count() == 0) {
+      return;
+    }
+
+    if (auto buf = dynamic_cast<dai::ImgFrame *>(callback.get())) {
+      sensor_msgs::msg::CompressedImage image;
+      image.data = std::move(buf->getData());
+      image.format = "jpeg";
+      image.header.stamp = node->now();
+      image.header.frame_id = "cam";
+
+      compressed_image_pub->publish(image);
+    }
+  });
+
+  preview_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (auto buf = dynamic_cast<dai::ImgFrame *>(callback.get())) {
+      sensor_msgs::msg::Image image;
+      image.data = std::move(buf->getData());
+      image.width = buf->getWidth();
+      image.height = buf->getHeight();
+      image.step = buf->getWidth() * 3;
+      image.encoding = "bgr8";
+      image.is_bigendian = false;
+      image.header.stamp = node->now();
+      image.header.frame_id = "cam";
+
+      preview_pub->publish(image);
+    }
+  });
+
+  mono_encoder_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (auto buf = dynamic_cast<dai::ImgFrame *>(callback.get())) {
+      sensor_msgs::msg::CompressedImage image;
+      image.data = std::move(buf->getData());
+      image.format = "jpeg";
+      image.header.stamp = node->now();
+      image.header.frame_id = "cam";
+
+      compressed_mono_pub->publish(image);
+    }
+  });
+
+  depth_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (auto buf = dynamic_cast<dai::ImgFrame *>(callback.get())) {
+      sensor_msgs::msg::Image image;
+      image.data = std::move(buf->getData());
+      image.width = buf->getWidth();
+      image.height = buf->getHeight();
+      image.step = image.width * 2;
+      image.encoding = "mono16";
+      image.is_bigendian = false;
+      image.header.stamp = node->now();
+      image.header.frame_id = "cam";
+
+      depth_pub->publish(image);
+    }
+  });
+
+  nnet_data_qeue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    RCLCPP_INFO(node->get_logger(), "NN DATA");
+    // if (auto nn_data = dynamic_cast<dai::NNData *>(callback.get())) {
+    //   for (const auto &name : nn_data->getAllLayerNames()) {
+    //     RCLCPP_INFO(node->get_logger(), "NN DATA ok!!! %s", name.c_str());
+
+    //     dai::TensorInfo tensor;
+    //     if (nn_data->getLayer("detection_out", &tensor)) {
+    //       tensor.
+    //     }
+    //   }
+    // }
+    if (auto buf = dynamic_cast<dai::ImgDetections *>(callback.get())) {
+
+      for (const auto &detection : buf->detections) {
+        RCLCPP_INFO(node->get_logger(), "Detection %i", detection.label);
+      }
+    }
+  });
+
+  tracker_queue->addCallback([&](std::shared_ptr<dai::ADatatype> callback) {
+    if (auto buf = dynamic_cast<dai::Tracklets *>(callback.get())) {
+      depthai_ros_msgs::msg::SpatialDetectionArray detections;
+      detections.header.frame_id = "cam";
+      detections.header.stamp = node->now();
+      for (const auto &tracklet : buf->tracklets) {
+        depthai_ros_msgs::msg::SpatialDetection detection;
+        detection.position.x = tracklet.spatialCoordinates.x;
+        detection.position.y = tracklet.spatialCoordinates.y;
+        detection.position.z = tracklet.spatialCoordinates.z;
+        detection.is_tracking = tracklet.status == dai::Tracklet::TrackingStatus::TRACKED;
+        detection.bbox.center.position.x = (tracklet.roi.x + tracklet.roi.width) / 2.0f;
+        detection.bbox.center.position.y = (tracklet.roi.y + tracklet.roi.height) / 2.0f;
+        detection.bbox.size_x = tracklet.roi.width;
+        detection.bbox.size_y = tracklet.roi.height;
+
+        detections.detections.push_back(detection);
+      }
+
+      object_tracker_pub->publish(detections);
+    }
+  });
 
   RCLCPP_INFO(node->get_logger(), "spin...");
   rclcpp::spin(node);
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -2,41 +2,119 @@
 
 #include "depthai/depthai.hpp"
 
+static const std::vector<std::string> label_map = {
+    "background", "aeroplane", "bicycle",     "bird",  "boat",        "bottle", "bus",
+    "car",        "cat",       "chair",       "cow",   "diningtable", "dog",    "horse",
+    "motorbike",  "person",    "pottedplant", "sheep", "sofa",        "train",  "tvmonitor"};
+
 dai::Pipeline createPipeline(const std::string &nn_path) {
   dai::Pipeline pipeline;
+
+  const float fps = 30.0f;
+
+  // sys logger (cpu usage, temperature etc.)
+  auto sys_logger = pipeline.create<dai::node::SystemLogger>();
 
   // color
   auto color = pipeline.create<dai::node::ColorCamera>();
   color->setPreviewSize(300, 300);
-  color->setResolution(
-      dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+  color->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
   color->setInterleaved(false);
   color->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
-  color->setFps(30);
+  color->setBoardSocket(dai::CameraBoardSocket::RGB);
+  color->setFps(fps);
 
-  // depth
-  //   auto depth = pipeline.create<dai::node::StereoDepth>();
-  //   depth->setDefaultProfilePreset(
-  //       dai::node::StereoDepth::PresetMode::HIGH_DENSITY);
-  //   depth->initialConfig.setMedianFilter(
-  //       dai::MedianFilter::KERNEL_5x5); // in hardware -> fast
+  // video encoder
+  auto color_encoder = pipeline.create<dai::node::VideoEncoder>();
+  color_encoder->setDefaultProfilePreset(fps, dai::VideoEncoderProperties::Profile::MJPEG);
+
+  auto mono_encoder = pipeline.create<dai::node::VideoEncoder>();
+  mono_encoder->setDefaultProfilePreset(fps, dai::VideoEncoderProperties::Profile::MJPEG);
+
+  // depth / stereo
+  auto mono_res = dai::node::MonoCamera::Properties::SensorResolution::THE_400_P;
+  auto mono_left = pipeline.create<dai::node::MonoCamera>();
+  mono_left->setResolution(mono_res);
+  mono_left->setBoardSocket(dai::CameraBoardSocket::LEFT);
+  mono_left->setFps(fps);
+  auto mono_right = pipeline.create<dai::node::MonoCamera>();
+  mono_right->setResolution(mono_res);
+  mono_right->setBoardSocket(dai::CameraBoardSocket::RIGHT);
+  mono_right->setFps(fps);
+
+  auto stereo = pipeline.create<dai::node::StereoDepth>();
+  stereo->setDefaultProfilePreset(dai::node::StereoDepth::PresetMode::HIGH_DENSITY);
+  stereo->initialConfig.setMedianFilter(dai::MedianFilter::KERNEL_7x7); // in hardware -> fast
+  //   auto stereo_config = stereo->initialConfig.get();
+  //   stereo_config.postProcessing.spatialFilter.alpha = 0.5;
+  //   stereo_config.postProcessing.spatialFilter.delta = 0;
+  //   stereo_config.postProcessing.spatialFilter.numIterations = 1;
+  //   stereo_config.postProcessing.spatialFilter.enable = true;
+  //   stereo_config.postProcessing.spatialFilter.holeFillingRadius = 3;
+  //   stereo->initialConfig.set(stereo_config);
+  stereo->setExtendedDisparity(true);
+  stereo->setLeftRightCheck(true);
+  stereo->setDepthAlign(dai::CameraBoardSocket::RGB);
+  stereo->setOutputSize(mono_left->getResolutionWidth(), mono_left->getResolutionHeight());
+
+  mono_left->out.link(stereo->left);
+  mono_right->out.link(stereo->right);
 
   // detection network
   auto dn = pipeline.create<dai::node::MobileNetDetectionNetwork>();
   dn->setConfidenceThreshold(0.5f);
   dn->setBlobPath(nn_path);
+  dn->input.setBlocking(false);
+
+  color->preview.link(dn->input);
+
+  // object tracker
+  auto object_tracker = pipeline.create<dai::node::ObjectTracker>();
+  object_tracker->setDetectionLabelsToTrack({15}); // people
+  object_tracker->setTrackerType(dai::TrackerType::ZERO_TERM_COLOR_HISTOGRAM);
+  object_tracker->setTrackerIdAssignmentPolicy(dai::TrackerIdAssignmentPolicy::SMALLEST_ID);
+  object_tracker->inputDetectionFrame.setBlocking(false);
+  object_tracker->inputDetections.setBlocking(false);
 
   // outputs
-  auto color_out = pipeline.create<dai::node::XLinkOut>();
+  auto sys_logger_out = pipeline.create<dai::node::XLinkOut>();
+  sys_logger_out->setStreamName("sys_logger");
+  auto preview_out = pipeline.create<dai::node::XLinkOut>();
+  preview_out->setStreamName("preview");
   auto nn_out = pipeline.create<dai::node::XLinkOut>();
-
-  color_out->setStreamName("preview");
   nn_out->setStreamName("detections");
+  auto color_mjpeg_out = pipeline.create<dai::node::XLinkOut>();
+  color_mjpeg_out->setStreamName("color_mjpeg");
+  auto mono_mjpeg_out = pipeline.create<dai::node::XLinkOut>();
+  mono_mjpeg_out->setStreamName("mono_mjpeg");
+  auto depth_out = pipeline.create<dai::node::XLinkOut>();
+  depth_out->setStreamName("depth");
+  auto tracker_out = pipeline.create<dai::node::XLinkOut>();
+  tracker_out->setStreamName("object_tracker");
 
-  // Link plugins CAM -> NN -> XLINK
-  color->preview.link(dn->input);
-  dn->passthrough.link(color_out->input);
+  // create links
+  sys_logger->out.link(sys_logger_out->input);
+  // dn->outNetwork.link(nn_out->input);
   dn->out.link(nn_out->input);
+  //   dn->passthrough.link(color_out->input);
+  //   dn->passthrough.link(nn_out->input);
+  color->preview.link(preview_out->input);
+
+  // link object tracker
+  // color->preview.link(object_tracker->inputTrackerFrame);
+  // dn->out.link(object_tracker->inputDetections);
+  // object_tracker->out.link(tracker_out->input);
+
+  // link color video encoder
+  color->video.link(color_encoder->input);
+  color_encoder->bitstream.link(color_mjpeg_out->input);
+
+  // link mono video encoder
+  mono_left->out.link(mono_encoder->input);
+  mono_encoder->bitstream.link(mono_mjpeg_out->input);
+
+  // link stereo out
+  stereo->depth.link(depth_out->input);
 
   return pipeline;
 }
